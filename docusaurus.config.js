65196efff8b4e88712910e486b637054f568a7cf
Kim-Heeyeol <khere220@gmail.com>
2022-02-28 22:49:37 +0900

    Add: url 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Modern JavaScript",
   tagline: "Deep Dive",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
+  url: "https://deep-dive.github.io",
+  baseUrl: "/deep-dive/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "Kim-Heeyeol", // Usually your GitHub org/user name.
+  projectName: "deep-dive", // Usually your repo name.
 
   presets: [
     [
