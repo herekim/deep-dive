6fdb768efee6ea5e2ad08652135bcc7edd82f0b1
heeyeol-kim <khere220@gmail.com>
2022-03-22 22:50:20 +0900

    Add: test ci/cd

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   favicon: "img/favicon.ico",
   organizationName: "herekim", // Usually your GitHub org/user name.
   projectName: "deep-dive", // Usually your repo name.
+  trailingSlash: false,
   //
 
   presets: [
